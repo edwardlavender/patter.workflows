d455b2c65d34c63bce5bdd346f922b3a2dc6d46c
edwardlavender <edward.lavender@eawag.ch>
2025-04-15 15:43:50 +0200

    Add simulated datasets

--- R/dats.R
+++ R/dats.R
@@ -1,8 +1,19 @@
 #' @title Datasets
-#' @description These functions load selected datasets. 
+#' @description This page describes package datasets.
 #' @details
-#' * [`dat_gebco_tm()`] loads a `TransitionLayer` object from `actel::transitionLayer()`. This is predefined to streamline examples. 
+#' * [`dat_sim_moorings`] is a [`data.table`] of simulated receiver locations (see [`patter::dat_moorings`]);
+#' * [`dat_sim_detections`]is a [`data.table`] of simulated detections (see [`patter::dat_moorings`]);
+#' * [`dat_gebco_tm()`] loads a `TransitionLayer` object from [`actel::transitionLayer()`]. This is predefined to streamline examples. 
 #' @author Edward Lavender
+#' @name dats
+
+#' @rdname dats
+"dat_sim_moorings"
+
+#' @rdname dats
+"dat_sim_detections"
+
+#' @rdname dats
 #' @export
 
 dat_gebco_tm <- function() {
--- data-raw/add-data.R
+++ data-raw/add-data.R
@@ -18,14 +18,67 @@ rm(list = ls())
 
 #### Essential packages
 library(patter)
+library(data.table)
+library(dtplyr)
+library(dplyr, warn.conflicts = FALSE)
 
 
 #########################
 #########################
-#### Define datasets
+#### Simulate tracking datasets
 
-# Define map
+# Connect to Julia
+julia_connect()
+set_seed()
+
+# Define study timeline 
+# * Use a one-week timeline for example speed
+start <- as.POSIXct("2025-04-13 00:00:00", tz = "UTC")
+end   <- as.POSIXct("2025-04-19 23:58:00", tz = "UTC")
+timeline <- seq(start, end, by = "2 mins")
+unique(lubridate::floor_date(timeline, "weeks"))
+
+# Define study area 
 map <- dat_gebco()
+set_map(map)
+
+# Simulate a dense receiver network
+# * For examples (e.g., with RSP), we need plenty of detections 
+dat_sim_moorings <- sim_array(.map = map, 
+                              .timeline = timeline,
+                              .arrangement = "regular",
+                              .n_receiver = 100L)
+
+# For two individuals, simulate a movement trajectory & acoustic observations
+dat_sim_detections <- 
+  lapply(1:2, function(id) {
+    
+    # Simulate path 
+    path <- sim_path_walk(.map = map, 
+                          .timeline = timeline, 
+                          .state = "StateXY", 
+                          .model_move = model_move_xy())
+    
+    # Simulate acoustic observations
+    obs <- sim_observations(.timeline = timeline, 
+                            .model_obs = model_obs_acoustic_logis_trunc(dat_sim_moorings))
+    
+    # Extract detections, following patter::dat_detections format
+    obs$ModelObsAcousticLogisTrunc[[1]] |> 
+      filter(obs == 1L) |>
+      mutate(individual_id = id) |> 
+      select("individual_id", "timestamp", receiver_id = "sensor_id") |> 
+      as.data.table()
+  }) |> 
+  rbindlist()
+
+# Examine detections data.table
+head(dat_sim_detections)
+
+
+#########################
+#########################
+#### Define ancillary datasets
 
 # Define transition matrix for RSP::runRSP() examples
 # * We predefine this to avoid Julia/terra issues with actel::transitionLayer()
@@ -39,6 +92,8 @@ dat_gebco_tm <-
 
 # Write to inst/
 saveRDS(dat_gebco_tm, file.path("inst", "extdata", "dat_gebco_tm.rds"))
+usethis::use_data(dat_sim_moorings, overwrite = TRUE)
+usethis::use_data(dat_sim_detections, overwrite = TRUE)
 
 
 #### End of code.
